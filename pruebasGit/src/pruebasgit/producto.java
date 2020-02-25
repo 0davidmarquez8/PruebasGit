7c7f548bf5bfdfeed8933288ab7d1e71c1725f9a
Camilo Vargas <kmilovargas913@gmail.com>
2020-02-24 22:05:02 -0500

    nueva clase

---
+++ pruebasGit/src/pruebasgit/producto.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebasgit;
+
+/**
+ *
+ * @author PROFESIONAL
+ */
+public class producto {
+    private String nombre;
+    private int precio;
+}
